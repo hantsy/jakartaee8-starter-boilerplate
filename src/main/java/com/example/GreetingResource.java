cda936c48d4082825444a09ef054cb32a6173819
hantsy <hantsy@gmail.com>
2019-09-27 21:33:39 +0800

    clean GreetingResource

--- src/main/java/com/example/GreetingResource.java
+++ src/main/java/com/example/GreetingResource.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import sun.util.resources.LocaleData;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.time.LocalDate;
 
 import static javax.ws.rs.core.Response.ok;
 
