105be07ab55bea459f1bade247433c3d37b2cc48
hantsy <hantsy@gmail.com>
2019-09-28 14:46:44 +0800

    add arquillian test for glassfish embedded.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# jakartaee8-starter
-A boilerplate project for starting a Jakarta EE 8 application in secodes
+# Jakarta EE 8 Starter
+A boilerplate project for starting a Jakarta EE 8 application in seconds.
+
+
+
+
+
+## Resources
+
+* [Deploying to Payara Server Using the Maven Cargo Plugin](https://blog.payara.fish/deploying-to-payara-server-using-the-maven-cargo-plugin)  by Payara Blog
+
+* [Cargo Maven2 Plugin for Glassfish v5](https://codehaus-cargo.github.io/cargo/GlassFish+5.x.html)
---
+++ src/test/java/com/example/GreetingServiceArqTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Arquillian.class)
+public class GreetingServiceArqTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(GreetingService.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    GreetingService service;
+
+    @Test
+    public void should_create_greeting() {
+        GreetingMessage message = service.buildGreetingMessage("Hantsy");
+        assertTrue("message should start with \"Say Hello to Hantsy at \"", message.getMessage().startsWith("Say Hello to Hantsy at "));
+    }
+}
